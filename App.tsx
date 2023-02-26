27210c20fa76a915a8914b5017a13f0f0600f657
lednewcrk <rwendelpereira@gmail.com>
2023-02-26 20:17:13 -0300

    feat: configure paper theme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,21 @@
+import { DefaultTheme, Provider } from 'react-native-paper'
+import { ThemeProp } from 'react-native-paper/lib/typescript/types'
 import { Router } from './src/navigation/router'
+import { Colors } from './src/values/colors'
+
+const theme: ThemeProp = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: Colors.background,
+        secondaryContainer: 'transparent'
+    }
+}
 
 export default function App() {
-    return Router()
+    return (
+        <Provider theme={theme}>
+            <Router />
+        </Provider>
+    )
 }
