b9926574cadf96491f00d81401a400feabf1ca24
lednewcrk <rwendelpereira@gmail.com>
2023-03-10 08:37:17 -0300

    feat: change http client base url by platform os

--- src/services/api/httpClient.ts
+++ src/services/api/httpClient.ts
@@ -1,5 +1,7 @@
+import { Platform } from 'react-native'
 import axios from 'axios'
 
 export const httpClient = axios.create({
-    baseURL: 'http://127.0.0.1:3000'
+    baseURL:
+        Platform.OS === 'ios' ? 'http://127.0.0.1:3000' : 'http://10.0.2.2:3000'
 })
